5e61f5ce0113e7757a1e465fe7301fad51b57d80
Juan Pablo Avendaño Pino <65789030+juanpablo2025@users.noreply.github.com>
2023-02-16 10:39:42 -0500

    setter and getters

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,11 +2,36 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero;
-    double peso=1;
+     private String nombre;
+     private String genero;
+    private double peso=1;
+    
     Animal pareja;
 
+    public Animal() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
 
     public Animal(String nombre, double peso) {
         this.nombre = nombre;
